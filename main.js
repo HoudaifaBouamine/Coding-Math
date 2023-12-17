dbdbd80f1640bfd746c5a4f31ec3fa8ade3e8cd3
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-17 19:39:16 +0100

    Center the graph

--- main.js
+++ main.js
@@ -5,19 +5,22 @@ window.onload = function(){
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
 
+    var c_width = width * 0.5;
+    var c_heigth = height * 0.5;
 
-    for(var i =0;i<= Math.PI * 10;i+= 0.001){
+    for(var i = - c_width / 50;i<= c_width / 50;i+= 0.001){
 
         var x = i * 50;
-        var y = -1 * (Math.cos(i) * 50) + width/2;
+        var y = -1 * (Math.cos(i) * 50);
         
-        context.fillRect(x,y,2,2);
+        context.fillRect(x + c_width,y + c_heigth,2,2);
         
-        y = -1 * (Math.sin(i) * 50) + width/2;
-        context.fillRect(x,y,2,2);
+        y = -1 * (Math.sin(i) * 50) ;
+        context.fillRect(x + c_width,y + c_heigth,2,2);
         
-        y = -1 * (Math.tan(i) * 50) + width/2;
-        context.fillRect(x,y,2,2);
+        y = -1 * (Math.tan(i) * 50) ;
+        context.fillRect(x + c_width,y + c_heigth,2,2);
+        
         
     }
 
