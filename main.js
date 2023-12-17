fcf3158d2f4e979406b98ab465f15b2006715748
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-17 19:35:39 +0100

    Coding sin(x) & tan(x)

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ window.onload = function(){
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
 
-    for(var i =0;i<= Math.PI * 10;i+= 0.01){
-        console.log("cos(" + i + ") = " + Math.cos(i));
+
+    for(var i =0;i<= Math.PI * 10;i+= 0.001){
+
         var x = i * 50;
         var y = -1 * (Math.cos(i) * 50) + width/2;
-
+        
         context.fillRect(x,y,2,2);
+        
+        y = -1 * (Math.sin(i) * 50) + width/2;
+        context.fillRect(x,y,2,2);
+        
+        y = -1 * (Math.tan(i) * 50) + width/2;
+        context.fillRect(x,y,2,2);
+        
     }
 
 };
