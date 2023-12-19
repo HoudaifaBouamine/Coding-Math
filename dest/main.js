e801da0ff94ac0bf2b23c1d0346a97f6e46d54cb
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-19 22:30:17 +0100

    tracing the cos,sin (x,y) and draw more lines for the origin

--- dest/main.js
+++ dest/main.js
@@ -2,26 +2,46 @@
 window.onload = function () {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
+    var info = document.getElementById("info");
     var width = canvas.width = window.innerWidth;
-    var height = canvas.height = window.innerHeight;
+    var height = canvas.height = window.innerHeight - 30;
     var x = 0, y = 0, angle = 0, speed = 0.01, radius = 200, xCenter = width / 2, yCenter = height / 2, frame = 0;
     render();
     function render() {
         angle += speed;
         context.clearRect(0, 0, width, height);
-        draw_origin(context, width, height, xCenter, yCenter);
+        draw_origin(context, width, height, xCenter, yCenter, radius / 5, radius / 5, 5, 5);
         x = radius * Math.cos(angle) + xCenter;
         y = -1 * radius * Math.sin(angle) + yCenter;
+        context.lineWidth = 1;
         context.beginPath();
-        context.arc(x, y, 10, 0, 2 * Math.PI);
+        context.moveTo(xCenter, yCenter);
+        context.lineTo(x, y);
+        context.strokeStyle = "red";
+        context.stroke();
+        context.beginPath();
+        context.moveTo(x, yCenter);
+        context.lineTo(x, y);
+        context.strokeStyle = "blue";
+        context.stroke();
+        context.beginPath();
+        context.moveTo(xCenter, y);
+        context.lineTo(x, y);
+        context.strokeStyle = "green";
+        context.stroke();
+        context.beginPath();
+        context.arc(x, y, 5, 0, 2 * Math.PI);
         context.fill();
-        var info = document.getElementById("info");
         if (frame % 5 == 0)
             info.innerHTML = `  x : ${(Math.cos(angle)).toFixed(2)}, y : ${(Math.sin(angle)).toFixed(2)}`;
         frame++;
         requestAnimationFrame(render);
     }
-    function draw_origin(context, max_width, max_height, xOrigin, yOrigin) {
+    function draw_origin(context, max_width, max_height, xOrigin, yOrigin, xStep = 30, yStep = 30, xBigStepNum = -1, yBigStepNum = -1) {
+        let oldLineWidth = context.lineWidth;
+        let oldColor = context.strokeStyle;
+        context.lineWidth = 2;
+        context.strokeStyle = "black";
         context.beginPath();
         context.moveTo(0, yOrigin);
         context.lineTo(width, yOrigin);
@@ -30,5 +50,67 @@ window.onload = function () {
         context.moveTo(xOrigin, 0);
         context.lineTo(xOrigin, height);
         context.stroke();
+        context.lineWidth = 1;
+        context.strokeStyle = "gray";
+        for (let i = xOrigin + xStep; i < width; i += xStep) {
+            context.beginPath();
+            context.moveTo(i, 0);
+            context.lineTo(i, height);
+            context.stroke();
+        }
+        for (let i = xOrigin - xStep; i >= 0; i -= xStep) {
+            context.beginPath();
+            context.moveTo(i, 0);
+            context.lineTo(i, height);
+            context.stroke();
+        }
+        for (let i = yOrigin + yStep; i < height; i += yStep) {
+            context.beginPath();
+            context.moveTo(0, i);
+            context.lineTo(width, i);
+            context.stroke();
+        }
+        for (let i = yOrigin - yStep; i >= 0; i -= yStep) {
+            context.beginPath();
+            context.moveTo(0, i);
+            context.lineTo(width, i);
+            context.stroke();
+        }
+        if (xBigStepNum != -1) {
+            let oldwidth = context.lineWidth;
+            context.lineWidth = 2;
+            for (let i = xOrigin + xStep * xBigStepNum; i < width; i += xStep * xBigStepNum) {
+                context.beginPath();
+                context.moveTo(i, 0);
+                context.lineTo(i, height);
+                context.stroke();
+            }
+            for (let i = xOrigin - xStep * xBigStepNum; i >= 0; i -= xStep * xBigStepNum) {
+                context.beginPath();
+                context.moveTo(i, 0);
+                context.lineTo(i, height);
+                context.stroke();
+            }
+            context.lineWidth = oldwidth;
+        }
+        if (yBigStepNum != -1) {
+            let oldwidth = context.lineWidth;
+            context.lineWidth = 2;
+            for (let i = yOrigin + yStep * yBigStepNum; i < width; i += yStep * yBigStepNum) {
+                context.beginPath();
+                context.moveTo(0, i);
+                context.lineTo(width, i);
+                context.stroke();
+            }
+            for (let i = yOrigin - yStep * yBigStepNum; i >= 0; i -= yStep * yBigStepNum) {
+                context.beginPath();
+                context.moveTo(0, i);
+                context.lineTo(width, i);
+                context.stroke();
+            }
+            context.lineWidth = oldwidth;
+        }
+        context.lineWidth = oldLineWidth;
+        context.strokeStyle = oldColor;
     }
 };
--- source/main.ts
+++ source/main.ts
@@ -1,15 +1,17 @@
 
 window.onload = function(){
-
+    
     var canvas : HTMLCanvasElement = 
-                    <HTMLCanvasElement>
-                     document.getElementById("canvas");
+    <HTMLCanvasElement>
+    document.getElementById("canvas");
     var context : CanvasRenderingContext2D =
-                     <CanvasRenderingContext2D> 
-                     canvas.getContext("2d");
+    <CanvasRenderingContext2D> 
+    canvas.getContext("2d");
+    
+    var info : HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("info");
 
-    var width   = canvas.width = window.innerWidth;
-    var height  = canvas.height = window.innerHeight;
+    var width   = canvas.width = window.innerWidth ;
+    var height  = canvas.height = window.innerHeight - 30;
 
     var x = 0,
         y = 0,
@@ -27,16 +29,35 @@ window.onload = function(){
         
         angle+=speed;
         context.clearRect(0,0,width,height);
-        draw_origin(context,width,height,xCenter,yCenter);
+
+        draw_origin(context,width,height,xCenter,yCenter,radius/5,radius/5,5,5);
 
         x = radius * Math.cos(angle) + xCenter;
         y = -1 * radius * Math.sin(angle) + yCenter;
-
-        context.beginPath();
-        context.arc(x,y,10,0,2 * Math.PI);
-        context.fill();
-        var info : HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("info");
         
+        context.lineWidth = 1;
+        context.beginPath();
+        context.moveTo(xCenter,yCenter);
+        context.lineTo(x,y); 
+        context.strokeStyle = "red" ;
+        context.stroke();
+        
+        context.beginPath();
+        context.moveTo(x,yCenter);
+        context.lineTo(x,y);
+        context.strokeStyle = "blue";
+        context.stroke();
+        
+        context.beginPath();
+        context.moveTo(xCenter,y);
+        context.lineTo(x,y);
+        context.strokeStyle = "green";
+        context.stroke();
+        
+        context.beginPath();
+        context.arc(x,y,5,0,2 * Math.PI);
+        context.fill();
+
         if(frame % 5 == 0)
             info.innerHTML = `  x : ${(Math.cos(angle)).toFixed(2)}, y : ${(Math.sin(angle)).toFixed(2)}`;
         frame++;
@@ -44,17 +65,99 @@ window.onload = function(){
         requestAnimationFrame(render);
     }
 
-    function draw_origin(context : CanvasRenderingContext2D,max_width:number,max_height:number,xOrigin : number,yOrigin : number){
+    function draw_origin(context : CanvasRenderingContext2D,max_width:number,max_height:number,xOrigin : number,yOrigin : number,xStep:number = 30,yStep:number = 30,xBigStepNum = -1,yBigStepNum=-1){
 
+        let oldLineWidth = context.lineWidth;
+        let oldColor = context.strokeStyle;
+        
+        context.lineWidth = 2;
+        context.strokeStyle = "black";
+        
+        context.beginPath();
+        context.moveTo(0,yOrigin);
+        context.lineTo(width,yOrigin);
+        context.stroke();
+
+        context.beginPath();
+        context.moveTo(xOrigin,0);
+        context.lineTo(xOrigin,height);
+        context.stroke();
+        
+        context.lineWidth = 1;
+        context.strokeStyle = "gray";
+
+        for(let i = xOrigin + xStep ; i < width;i+=xStep){
             context.beginPath();
-            context.moveTo(0,yOrigin);
-            context.lineTo(width,yOrigin);
+            context.moveTo(i,0);
+            context.lineTo(i,height);
             context.stroke();
+        }
+        for(let i = xOrigin - xStep ; i >= 0;i-=xStep){
             context.beginPath();
-            context.moveTo(xOrigin,0);
-            context.lineTo(xOrigin,height);
+            context.moveTo(i,0);
+            context.lineTo(i,height);
             context.stroke();
-            
+        }
+        
+        for(let i = yOrigin + yStep ; i < height;i+=yStep){
+            context.beginPath();
+            context.moveTo(0,i);
+            context.lineTo(width,i);
+            context.stroke();
+        }
+        for(let i = yOrigin -yStep ; i >= 0;i-=yStep){
+            context.beginPath();
+            context.moveTo(0,i);
+            context.lineTo(width,i);
+            context.stroke();
+        }
+
+        if(xBigStepNum != -1)
+        {
+            let oldwidth = context.lineWidth;
+            context.lineWidth = 2;
+            for(let i = xOrigin + xStep * xBigStepNum ; i < width;i+=xStep * xBigStepNum){
+                context.beginPath();
+                context.moveTo(i,0);
+                context.lineTo(i,height);
+
+
+                context.stroke();
+            }
+
+            for(let i = xOrigin - xStep * xBigStepNum ; i >= 0;i-=xStep * xBigStepNum){
+                context.beginPath();
+                context.moveTo(i,0);
+                context.lineTo(i,height);
+                context.stroke();
+            }
+
+            context.lineWidth = oldwidth;
+        }
+        if(yBigStepNum != -1)
+        {
+            let oldwidth = context.lineWidth;
+            context.lineWidth = 2;
+            for(let i = yOrigin + yStep * yBigStepNum ; i < width;i+=yStep * yBigStepNum){
+                context.beginPath();
+                context.moveTo(0,i);
+                context.lineTo(width,i);
+                context.stroke();
+            }
+
+            for(let i = yOrigin - yStep * yBigStepNum ; i >= 0;i-=yStep * yBigStepNum){
+                context.beginPath();
+                context.moveTo(0,i);
+                context.lineTo(width,i);
+                context.stroke();
+            }
+
+            context.lineWidth = oldwidth;
+        }
+
+        context.lineWidth   = oldLineWidth ;
+        context.strokeStyle = oldColor; 
+        
     }
     
 };
