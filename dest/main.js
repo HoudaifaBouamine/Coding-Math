364606cda62b0914023a6b411ece09aa97a69677
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-19 21:30:43 +0100

    Using TS

--- dest/main.js
+++ dest/main.js
@@ -1,37 +1,22 @@
-
-window.onload = function(){
-    
+"use strict";
+window.onload = function () {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
     var width = canvas.width = window.innerWidth;
     var height = canvas.height = window.innerHeight;
-    
     var c_width = width * 0.5;
     var c_heigth = height * 0.5;
-
     var moment = 100;
     var speed = 0.05;
-    var w = 20,h=20;
-    
+    var w = 20, h = 20;
     render();
-
-    function render(){
+    function render() {
         moment += speed;
-
-        context.clearRect(0,0,width,height);
-
-        context.fillRect(c_width - 20,c_heigth + 300 * Math.cos(moment),w,h);
-
+        context.clearRect(0, 0, width, height);
+        context.fillRect(c_width - 20, c_heigth + 300 * Math.cos(moment), w, h);
         context.beginPath();
-        context.arc(c_width + 20,400 - 100 * Math.cos(moment),30,0,Math.PI * 2,false);
+        context.arc(c_width + 20, 400 - 100 * Math.cos(moment), 30, 0, Math.PI * 2, false);
         context.fill();
         requestAnimationFrame(render);
-        
     }
-    
 };
-
-
-
-
-
---
+++ source/main.ts
@@ -0,0 +1,42 @@
+
+window.onload = function(){
+
+    var canvas : HTMLCanvasElement = 
+                    <HTMLCanvasElement>
+                     document.getElementById("canvas");
+    var context : CanvasRenderingContext2D =
+                     <CanvasRenderingContext2D> 
+                     canvas.getContext("2d");
+
+    var width   = canvas.width = window.innerWidth;
+    var height  = canvas.height = window.innerHeight;
+    
+    var c_width  = width * 0.5;
+    var c_heigth = height * 0.5;
+
+    var moment = 100;
+    var speed = 0.05;
+    var w = 20,h=20;
+    
+    render();
+
+    function render(){
+        moment += speed;
+
+        context.clearRect(0,0,width,height);
+
+        context.fillRect(c_width - 20,c_heigth + 300 * Math.cos(moment),w,h);
+
+        context.beginPath();
+        context.arc(c_width + 20,400 - 100 * Math.cos(moment),30,0,Math.PI * 2,false);
+        context.fill();
+        requestAnimationFrame(render);
+        
+    }
+    
+};
+
+
+
+
+
