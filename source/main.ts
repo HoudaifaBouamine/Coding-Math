6ca5cafdf32b279b56dc7cbb971c554b423efac4
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-12-27 12:31:30 +0100

    Draw waves out from circle

--- source/main.ts
+++ source/main.ts
@@ -1,172 +1,101 @@
 
-window.onload = function(){
+window.onload = render;
+
+
+function render(){
     
-    var canvas : HTMLCanvasElement = 
-    <HTMLCanvasElement>
-    document.getElementById("canvas");
-    var context : CanvasRenderingContext2D =
-    <CanvasRenderingContext2D> 
-    canvas.getContext("2d");
+    let canvas : HTMLCanvasElement = <HTMLCanvasElement>
+        document.getElementById("canvas");
+    let context : CanvasRenderingContext2D = <CanvasRenderingContext2D> 
+        canvas.getContext("2d");
     
-    var info : HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("info");
+    let width   = canvas.width = window.innerWidth ;
+    let height  = canvas.height = window.innerHeight - 30;
+    let time = 0;
+    let timeSpeed = 0.03;
+    let wave : number[] = []; 
+    
+    draw();
+    function draw(){
 
-    var width   = canvas.width = window.innerWidth ;
-    var height  = canvas.height = window.innerHeight - 30;
-
-    var x = 0,
-        y = 0,
-        xAngle = 0,
-        yAngle = 0,
-        ySpeed = 0.01,
-        xSpeed = 0.013,
-        xRadius = 150,
-        yRadius = 250,
-        xCenter = width/2,
-        yCenter = height/2,
-        frame = 0;
-
-
-        render();
+        context.fillStyle = 'black';
+        context.fillRect(0,0,width,height);
         
-    function render(){
+        let radius = 100;
         
-        xAngle+=xSpeed;
-        yAngle+=ySpeed;
-        context.clearRect(0,0,width,height);
-
-        draw_origin(context,width,height,xCenter,yCenter,xRadius/5,xRadius/5,5,5);
-
-        x = xRadius * Math.cos(xAngle) + xCenter;
-        y = -1 * yRadius * Math.sin(yAngle) + yCenter;
+        context.strokeStyle = 'white';
+        let x = radius + 40;
+        let y = radius + 40;
+        context.beginPath();
+        context.ellipse(x,y,radius,radius,0.5,0,Math.PI * 2)
+        context.stroke();
         
+        let angle = time;
+        let dot_x = x + Math.cos(angle) * radius;
+        let dot_y = y - Math.sin(angle) * radius;
+        let dot_radius = 5;
+        context.fillStyle = 'white';
+
+        dot(dot_x,dot_y,dot_radius);
+
+        line(x,y,dot_x,dot_y);
+        wave.push(dot_y);
+
+        context.beginPath();
         context.lineWidth = 1;
-        context.beginPath();
-        context.moveTo(xCenter,yCenter);
-        context.lineTo(x,y); 
-        context.strokeStyle = "red" ;
+        for(let i = 0 ; i < wave.length - 1;i+=1){
+            let wave_point_x = i + x + radius * 1.5;
+            let wave_point_y = wave[wave.length -1 - i];
+            
+            let wave_point_x_next = (i+1) + x + radius * 1.5;
+            let wave_point_y_next = wave[wave.length -1 - (i+1)];
+            
+            context.moveTo(wave_point_x,wave_point_y);
+            context.lineTo(wave_point_x_next,wave_point_y_next);
+        }
+        
         context.stroke();
-        
-        context.beginPath();
-        context.moveTo(x,yCenter);
-        context.lineTo(x,y);
-        context.strokeStyle = "blue";
-        context.stroke();
-        
-        context.beginPath();
-        context.moveTo(xCenter,y);
-        context.lineTo(x,y);
-        context.strokeStyle = "green";
-        context.stroke();
-        
-        context.beginPath();
-        context.arc(x,y,5,0,2 * Math.PI);
-        context.fill();
+        context.lineWidth=1;
 
-        if(frame % 5 == 0)
-            info.innerHTML = `  x : ${(Math.cos(xAngle)).toFixed(2)}, y : ${(Math.sin(yAngle)).toFixed(2)}`;
-        frame++;
-        
-        requestAnimationFrame(render);
-    }
+        let first_wave_point_x = x + radius * 1.5;
+        let first_wave_point_y = wave[wave.length -1];
 
-    function draw_origin(context : CanvasRenderingContext2D,max_width:number,max_height:number,xOrigin : number,yOrigin : number,xStep:number = 30,yStep:number = 30,xBigStepNum = -1,yBigStepNum=-1){
+        line(dot_x,dot_y,first_wave_point_x,first_wave_point_y);
 
-        let oldLineWidth = context.lineWidth;
-        let oldColor = context.strokeStyle;
-        
-        context.lineWidth = 2;
-        context.strokeStyle = "black";
-        
-        context.beginPath();
-        context.moveTo(0,yOrigin);
-        context.lineTo(width,yOrigin);
-        context.stroke();
+        dot(first_wave_point_x,first_wave_point_y,3);
 
-        context.beginPath();
-        context.moveTo(xOrigin,0);
-        context.lineTo(xOrigin,height);
-        context.stroke();
+        time+=timeSpeed;
         
-        context.lineWidth = 1;
-        context.strokeStyle = "gray";
-
-        for(let i = xOrigin + xStep ; i < width;i+=xStep){
-            context.beginPath();
-            context.moveTo(i,0);
-            context.lineTo(i,height);
-            context.stroke();
-        }
-        for(let i = xOrigin - xStep ; i >= 0;i-=xStep){
-            context.beginPath();
-            context.moveTo(i,0);
-            context.lineTo(i,height);
-            context.stroke();
-        }
-        
-        for(let i = yOrigin + yStep ; i < height;i+=yStep){
-            context.beginPath();
-            context.moveTo(0,i);
-            context.lineTo(width,i);
-            context.stroke();
-        }
-        for(let i = yOrigin -yStep ; i >= 0;i-=yStep){
-            context.beginPath();
-            context.moveTo(0,i);
-            context.lineTo(width,i);
-            context.stroke();
+        if(wave.length > 500){
+            wave.shift();
         }
 
-        if(xBigStepNum != -1)
-        {
-            let oldwidth = context.lineWidth;
-            context.lineWidth = 2;
-            for(let i = xOrigin + xStep * xBigStepNum ; i < width;i+=xStep * xBigStepNum){
-                context.beginPath();
-                context.moveTo(i,0);
-                context.lineTo(i,height);
-
-
-                context.stroke();
-            }
-
-            for(let i = xOrigin - xStep * xBigStepNum ; i >= 0;i-=xStep * xBigStepNum){
-                context.beginPath();
-                context.moveTo(i,0);
-                context.lineTo(i,height);
-                context.stroke();
-            }
-
-            context.lineWidth = oldwidth;
-        }
-        if(yBigStepNum != -1)
-        {
-            let oldwidth = context.lineWidth;
-            context.lineWidth = 2;
-            for(let i = yOrigin + yStep * yBigStepNum ; i < width;i+=yStep * yBigStepNum){
-                context.beginPath();
-                context.moveTo(0,i);
-                context.lineTo(width,i);
-                context.stroke();
-            }
-
-            for(let i = yOrigin - yStep * yBigStepNum ; i >= 0;i-=yStep * yBigStepNum){
-                context.beginPath();
-                context.moveTo(0,i);
-                context.lineTo(width,i);
-                context.stroke();
-            }
-
-            context.lineWidth = oldwidth;
-        }
-
-        context.lineWidth   = oldLineWidth ;
-        context.strokeStyle = oldColor; 
+        requestAnimationFrame(draw);
         
     }
     
+    function line(x1: number,y1: number,x2: number,y2 : number)
+    {
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+
+    }
+
+    function point(x:number,y:number)
+    {
+        context.beginPath();
+        context.moveTo(x,y);
+        context.lineTo(x+1,y+1);
+        context.stroke();
+    }
+
+    function dot(x:number,y:number,radius:number)
+    {
+        context.beginPath();
+        context.arc(x,y,radius,0,Math.PI * 2);
+        context.fill();    
+    }
 };
-
-
-
-
 
